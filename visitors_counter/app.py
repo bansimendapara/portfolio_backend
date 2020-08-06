3b6d2e4fbad4bddd5179914ffae4307e1bec12e4
bansimendapara <36428790+bansimendapara@users.noreply.github.com>
2020-08-06 16:27:24 -0700

    Update app.py

--- visitors_counter/app.py
+++ visitors_counter/app.py
@@ -34,10 +34,10 @@ def lambda_handler(event, context):
         "isBase64Encoded": False,
         "statusCode": 200,
         "body": responseBody,
-        "headers": {
-            "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,x-requested-with",
-            "Access-Control-Allow-Origin": "*",
-            "Access-Control-Allow-Methods": "GET,OPTIONS",
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
         },
     }
 
